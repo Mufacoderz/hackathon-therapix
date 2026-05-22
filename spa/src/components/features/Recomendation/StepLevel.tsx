4e24b9f8336a4bb4f18a40866d866ead2b3cb38d
indradev <indrawanismail0@gmail.com>
2026-05-22 15:06:17 +0800

    add level

--- spa/src/components/features/Recomendation/StepLevel.tsx
+++ spa/src/components/features/Recomendation/StepLevel.tsx
@@ -1,7 +1,11 @@
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { LuLeaf, LuWind } from "react-icons/lu";
+import { GiFist } from "react-icons/gi";
+
 const LEVELS = [
-  { id: "Lembut", label: "Lembut" },
-  { id: "Normal", label: "Normal" },
-  { id: "Keras", label: "Keras" },
+  { id: "Lembut", label: "Lembut", desc: 'Relaksasi Ringan', icon: <LuLeaf className="text-[35px] md:text-[60px]"  />  },
+  { id: "Normal", label: "Normal", desc: 'Seimbang & Nyaman', icon: <LuWind className="text-[35px] md:text-[60px]" />    },
+  { id: "Keras", label: "Keras", desc: 'Kuat & Intens', icon: <GiFist className="text-[35px] md:text-[60px]"  />   },
 ];
 
 interface Props {
@@ -12,33 +16,47 @@ interface Props {
 
 export default function StepLevel({ selected, onSelect, onNext }: Props) {
   return (
-    <div>
-      <h2 className="text-lg font-semibold text-stone-800 mb-1">Level pijatan</h2>
-      <p className="text-sm text-stone-500 mb-5">Pilih intensitas pijatan yang kamu nyaman</p>
+    <div className="font-poppins">
+      <h2 className="text-lg text-center font-semibold font-poppins text-[#8B6B52] mb-1">Seberapa Kuat Pijatan yang Kamu Inginkan</h2>
+      <p className="text-sm text-center text-[#C8A96E] mb-5">Pilih Level Pijatan</p>
 
-      <div className="flex flex-col gap-3 mb-6">
+      <div className="flex mt-12 gap-4 mb-6">
         {LEVELS.map((lvl) => (
           <button
             key={lvl.id}
             onClick={() => onSelect(lvl.id)}
-            className={`rounded-2xl border p-4 text-left transition-all ${
+            className={`rounded-4xl border border-[#8B6B52] text-center transition-all shadow-2xl w-58 h-38 md:h-64 ${
               selected === lvl.id
-                ? "border-stone-800 bg-stone-100"
-                : "border-stone-200 hover:border-stone-400"
+                ? " border-4 border-[#8B6B52] bg-stone-100"
+                : "border border-8B6B52 hover:border-[#8B6B52]"
             }`}
           >
-            <p className="text-sm font-medium text-stone-800">{lvl.label}</p>
+            <span className="text-[#8B6B52] flex justify-center items-center">{lvl.icon}</span>
+            <p className="text-sm md:text-md font-bold font-poppins text-[#8B6B52]">{lvl.label}</p>
+            <p className="text-[8px] md:text-sm font-light text-[#8B6B52]">{lvl.desc}</p>
           </button>
         ))}
       </div>
 
+      {/* footer step*/}
+<section className="flex items-center justify-between mt-22 md:mt-0">
       <button
         onClick={onNext}
-        disabled={!selected}
-        className="w-full py-3 rounded-2xl bg-stone-800 text-white text-sm font-medium hover:bg-stone-700 transition disabled:opacity-40"
-      >
-        Lanjut
+        disabled={selected.length === 0}
+        className="flex justify-center items-center gap-2 mt-6 w-28 md:w-32 py-2 rounded-4xl border-2 border-[#8B6B52] text-[#8B6B52] text-sm font-medium transition disabled:opacity-40"
+        >
+          <span><FaArrowLeft /></span>
+          <span>Kembali</span>
       </button>
+      <button
+        onClick={onNext}
+        disabled={selected.length === 0}
+        className="flex justify-center items-center gap-2 w-42 md:w-52 mt-6 py-2 rounded-4xl border-2 border-[#8B6B52] text-[#8B6B52] text-sm font-medium transition disabled:opacity-40"
+        >
+          <span>Lanjutkan</span>
+          <span><FaArrowRight /></span>
+      </button>
+        </section>
     </div>
   );
 }
